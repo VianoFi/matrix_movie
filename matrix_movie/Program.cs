7ad8bb610d454bb2374c9a5ea9f034ddd8326471
Filippo Viano <filippo.viano@itiscuneo.eu>
2025-10-30 13:27:46 +0100

    aggiunta Auth e ruoli, modifica layout

--- matrix_movie/Program.cs
+++ matrix_movie/Program.cs
@@ -1,6 +1,7 @@
-using matrix_movie.Data;
+﻿using matrix_movie.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TuoProgetto.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,27 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()  // ⬅️ AGGIUNGI QUESTA RIGA
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
+// AGGIUNGI QUESTA CONFIGURAZIONE
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+});
+
 var app = builder.Build();
+
+// Inizializza i ruoli - METTI QUESTO SOLO UNA VOLTA QUI
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await RoleInitializer.InitializeAsync(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,7 +42,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,7 +49,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
